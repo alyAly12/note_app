0e3be974190e69b3ad6d067f176dfb32838c0f8f
alyAly12 <medoaly870@gmail.com>
2022-12-07 16:05:01 +0200

    adding route and home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/home_view.dart';
 
 void main() {
   runApp(const NoteApp());
@@ -9,6 +10,12 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(),
+      initialRoute: HomeView.id,
+      routes: {
+        HomeView.id:(context)=>const HomeView()
+      },
+    );
   }
 }
---
+++ lib/views/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+static String id='homeView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
